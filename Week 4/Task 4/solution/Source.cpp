93f6f89c44eae9ddc0661ce7b26ec125507f8403
Insomnia2864 <52221605+Insomnia2864@users.noreply.github.com>
2019-12-25 16:30:34 +0300

    Add files via upload

---
+++ Week 4/Task 4/solution/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+void RemoveDuplicates(std::vector<T>& elements)
+{
+	std::sort(elements.begin(), elements.end());
+	auto it = std::unique(elements.begin(), elements.end());
+	elements.erase(it, elements.end());
+}
+
+int main() 
+{
+    std::vector<int> v1 = { 6, 4, 7, 6, 4, 4, 0, 1 };
+    RemoveDuplicates(v1);
+    for (int x : v1)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<std::string> v2 = { "C", "C++", "C++", "C", "C++" };
+    RemoveDuplicates(v2);
+    for (const std::string& s : v2) 
+    {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
